ee5ebb635afb86979f0f2c1514b163323383a70c
abubakar28oc77 <abubakar.28oc77@gmail.com>
2026-09-04 11:16:03 +0600

    Configure Gradle 8.10.2 with AGP 8.7.0 and android.newDsl=false for ABS Hisab Manager-2 APK build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,17 +13,17 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
         applicationId = "com.tutorfinance.finance"
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
